fdd249c7b60e1880034edf57dd14d177a1d87f6c
jam_mi <jam00@vip.qq.com>
2021-04-05 11:58:51 +0800

    添加 okex 链

--- src/buildList.js
+++ src/buildList.js
@@ -4,6 +4,8 @@ const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
 const kovan = require("./tokens/kovan.json");
+const okex = require("./tokens/okex.json");
+const okextest = require("./tokens/okextest.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -18,7 +20,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
     keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    tokens: [...okex, ...okextest]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
