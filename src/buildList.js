73d1f58e251f01933a3bd34694ba8aaf60d65f8e
jam_mi <jam00@vip.qq.com>
2021-04-05 12:00:35 +0800

    update gitignore

--- src/buildList.js
+++ src/buildList.js
@@ -1,9 +1,9 @@
 const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
+// const mainnet = require("./tokens/mainnet.json");
+// const ropsten = require("./tokens/ropsten.json");
+// const rinkeby = require("./tokens/rinkeby.json");
+// const goerli = require("./tokens/goerli.json");
+// const kovan = require("./tokens/kovan.json");
 const okex = require("./tokens/okex.json");
 const okextest = require("./tokens/okextest.json");
 
